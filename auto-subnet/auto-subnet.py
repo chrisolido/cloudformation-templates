39659f029cd44181fa3bfa97325386596da3df59
Tony Han <itony9401@live.com>
2018-11-28 21:26:07 -0800

    refactored auto-subnet lambda and template

--- auto-subnet/auto-subnet.py
+++ auto-subnet/auto-subnet.py
@@ -1,183 +1,160 @@
-import random, string, json
+"""
+Allocate Subnet CIDR within a VPC CIDR Block
+
+Uses DynamoDB for persistence of allocated CIDR
+"""
+import os
+import json
 from uuid import uuid4
 
 import boto3
-import requests
+
 from boto3.dynamodb.conditions import Key, Attr
-from netaddr import IPSet, IPNetwork
+import requests
+from netaddr import IPNetwork
 
-debug = ''
+table_name = os.getenv('DynamoDBTableName', None)
+region = os.getenv('DynamoDBRegion', '')
+vpc_cidr = os.getenv('VPCCidr', None)
 
-def sendresponse(event, context, responsestatus, responsedata, reason):
+dynamodb_table = boto3.resource('dynamodb', region_name=region).Table(table_name)
+
+
+def handler(event, context):
+    """Allocate 3 IP ranges from a VPC CIDR Block"""
+    print(f"Function ARN: {context.invoked_function_arn}")
+    print(f"Event: {json.dumps(event)}")
+    print(f"Context: {json.dumps(event)}")
+
+    stack_id = event['StackId']
+    request_type = event['RequestType']
+
+    # if deleting stack, remove the subnet allocationsfor this stack_id
+    if request_type == 'Delete':
+        print("Deleting Subnets...")
+        delete_subnets(event, stack_id)
+        exit(1)
+
+    ## otherwise allocate 4 ranges
+    print("Allocating Subnets...")
+    ipnet = IPNetwork(vpc_cidr)
+    subnet_mask = 24
+    subnets = ipnet.subnet(subnet_mask)
+    subnets_allocated = 0
+    number_to_allocate = 3
+
+    for subnet in subnets:
+        if subnets_allocated == number_to_allocate:
+            break
+        if cidr_is_reserved(subnet):
+            continue
+        if cidr_is_in_table(subnet):
+            continue
+
+        use_subnet(subnet, stack_id)  # store the allocation in the DB
+        print(f"Subnets Allocated {subnets_allocated} - {str(subnet)}")
+        subnets_allocated += 1
+
+    # read all the subnets for this stack from table
+    response = dynamodb_table.query(
+        IndexName='StackCidrRange',
+        KeyConditionExpression=Key('StackId').eq(stack_id))
+
+    if len(response['Items']) < number_to_allocate:
+        send_response(
+            event,
+            status='FAILED',
+            reason=f"Couldn't obtain {number_to_allocate} CIDR for stack from DynamoDB query"
+        )
+        exit(1)
+
+    print('Response from DynamoDB: ')
+    for item in response['Items']:
+        print(item)
+
+    try:
+        response_data = {
+            'AppPublicCIDR1': response['Items'][0]['Cidr'],
+            'AppPublicCIDR2': response['Items'][1]['Cidr'],
+            'AppPrivateCIDR': response['Items'][2]['Cidr'],
+        }
+        send_response(event, status='SUCCESS', data=response_data)
+
+    except Exception as err:
+        send_response(
+            event, status='FAILED', reason="Failed to generate response data")
+        print("Error: " + str(err))
+
+
+def use_subnet(cidr, stack_id):
+    """ Add the passed subnet into the DB"""
+    cidr = str(cidr)
+
+    dynamodb_table.put_item(Item={
+        'Cidr': cidr,
+        'StackId': str(stack_id),
+    })
+
+
+def delete_subnets(event, stack_id):
+    """ Delete any subnets in the DB for a stack"""
+    try:
+        response = dynamodb_table.scan(
+            FilterExpression=Attr('StackId').eq(stack_id))
+
+        print('Deleting items...')
+        for item in response['Items']:
+            print(item)
+            dynamodb_table.delete_item(Key={'Cidr': item['Cidr']})
+
+        send_response(event, status='SUCCESS')
+
+    except Exception as err:
+        send_response(
+            event,
+            status='FAILED',
+            reason='Failed to delete allocated subnets in DynamoDB')
+        print("Error: " + str(err))
+
+
+def send_response(event, status, reason="", data=None):
     """Send a Success or Failure event back to CFN stack"""
+    if data is None:
+        data = {}
+
+    if status == 'FAILED':
+        print(reason)
+
     payload = {
         'StackId': event['StackId'],
-        'Status' : responsestatus,
-        'Reason' : reason,
+        'Status': status,
+        'Reason': reason,
         'RequestId': event['RequestId'],
         'LogicalResourceId': event['LogicalResourceId'],
         'PhysicalResourceId': event['LogicalResourceId'] + str(uuid4()),
-        'Data': responsedata
+        'Data': data
     }
-    if debug == "True":
-        print("Sending {0} to {1}".format(json.dumps(payload), event['ResponseURL']))
-    requests.put(event['ResponseURL'], data=json.dumps(payload))
-    print("Sent %s to %s".format(json.dumps(payload), event['ResponseURL']))
+
+    json_payload = json.dumps(payload)
+
+    print(f"Sending {json_payload} to {event['ResponseURL']}")
+    requests.put(event['ResponseURL'], data=json_payload)
 
 
-
-def usesubnets(cidr, ddb_t, stack_id):
-    """ Add the passed subnet into the DB"""
-    ddb_t.put_item(Item={
-        'Cidr': str(cidr),
-        'StackId': str(stack_id),
-    })
-    return str(cidr)
-
-def deletesubnets(stack_id, ddb_t):
-    """ Delete any subnets in the DB for a stack"""
-    try:
-        response = ddb_t.scan(
-            FilterExpression=Attr('StackId').eq(stack_id)
-        )
-        for item in response['Items']:
-            print item
-            ddb_t.delete_item(Key={'Cidr' : item['Cidr']})
-    except Exception:
-        pass
-
-def is_cidr_in_table(cidr, ddb_t):
+def cidr_is_in_table(cidr):
     """ Check if the CIDR is already allocated """
     cidr = str(cidr)
-    response = ddb_t.query(
-        KeyConditionExpression=Key('Cidr').eq(cidr)
-    )
+    response = dynamodb_table.query(
+        KeyConditionExpression=Key('Cidr').eq(cidr))
     items = response['Items']
-    if len(items) > 0:
-        return True
-    return False
 
-def is_cidr_reserved(cidr):
+    return len(items) > 0
+
+
+def cidr_is_reserved(cidr):
     """ Check if CIDR is already reserved"""
     cidr = str(cidr)
     reserved1 = os.getenv('ReservedCIDR1', None)
     reserved2 = os.getenv('ReservedCIDR2', None)
 
-    return cidr == reserved1 or cidr == reserved2
-
-def handler(event, context):
-    """ Attempt to allocate 4 IP ranges from a VPC CIDR Block"""
-
-    print "Started execution of Autosubnet Lambda..."
-    print "Function ARN %s" % context.invoked_function_arn
-    print "Incoming Event %s " % json.dumps(event)
-    global debug
-
-    tablename = ''
-    stack_id = ''
-    region = ''
-    ddb_t = None
-    vpccidr = ''
-    requesttype = ''
-
-    try: # attempt to set debug status from CFN config - otherwise true
-        debug = event['ResourceProperties']['debug']
-    except Exception:
-        debug = "False"
-
-    if debug == "True": # Print context and event - only if debug
-        print event
-        print context
-
-    try: # get the stack ID from incoming event, if not-present FAIL
-        stack_id = event['StackId']
-    except Exception:
-        sendresponse(event, context, 'FAILED', {'Error': 'Cant determine stackid'}, "Cant determine stackid")
-        return
-
-    try: # Check that DynamoDB region has been provided by template, if not, FAIL
-        region = event['ResourceProperties']['DynamoDBRegion']
-    except Exception:
-        sendresponse(event, context, 'FAILED', {'Error': 'Region not provided.'}, "Region not provided.")
-        return
-
-    try: # Check that DynamoDB tablename has been provided by template, if not, FAIL
-        tablename = event['ResourceProperties']['DynamoDBTable']
-    except Exception:
-        sendresponse(event, context, 'FAILED', {'Error': 'Tablename not provided.'}, "Tablename not provided.")
-        return
-
-    try: # Check that sharedinfrastructure VPCCidr has been provided by template, if not, FAIL
-        vpccidr = event['ResourceProperties']['VPCCidr']
-    except Exception:
-        sendresponse(event, context, 'FAILED', {'Error': 'VPC Cidr not provided.'}, "VPC Cidr not provided.")
-        return
-
-    try: # Check that we can determine request type ... interested in CREATE or DELETE
-        requesttype = event['RequestType']
-    except Exception:
-        sendresponse(event, context, 'FAILED', {'Error': 'Cant determine request type.'}, "Cant determine request type.")
-
-    if debug == "True":
-        print "Past Input Checking"
-
-    # make sure we have all the required inputs to the function
-    # If function is still executing - we're all good on inputs
-    # Connect to the DDB Table
-    ddb_t = boto3.resource('dynamodb', region_name=region).Table(tablename)
-
-    if requesttype == 'Delete': # if delete, remove the subnet allocations, for this stack_id
-        if debug == 'True':
-            print "Delete Requesttype Processing Started"
-        deletesubnets(stack_id, ddb_t)
-        sendresponse(event, context, 'SUCCESS', {}, "")
-        return
-
-    ## otherwise allocate 4 ranges
-    if debug == "True":
-        print "Create Requesttype Processing Started"
-    subnetmask = 24 # size of networks to allocate - static for now
-    ipnet = IPNetwork(vpccidr)
-    subnetsallocated = 0
-
-    # Save possible client address ranges to an array
-    subnets = ipnet.subnet(subnetmask)
-    success = False
-
-    if debug == "True":
-        print "Starting Possible Subnet Iteration"
-    for subnet in subnets:
-        if subnetsallocated == 4:
-            success = True
-            break
-        if is_cidr_reserved(subnet, vpccidr):
-            continue
-        if is_cidr_in_table(subnet, ddb_t):
-            continue
-        subnetsallocated += 1
-        print "Subnets Allocated %d - %s" % (subnetsallocated, str(subnet))
-        usesubnets(subnet, ddb_t, stack_id) # store the allocation in the DB
-
-    responsedata = {} # dictionary to store our return data
-    # read all the subnets for this stack from table
-    response = ddb_t.query(
-        IndexName='rangesforstack',
-        KeyConditionExpression=Key('StackId').eq(stack_id)
-    )
-    if len(response['Items']) < 4:
-        success = False
-        sendresponse(event, context, 'FAILED', responsedata, "Cant read 4 CIDRs for stack from DDB")
-        exit(1)
-    if debug == "True":
-        for item in response['Items']:
-            print item
-    if success:
-        responsedata = {
-            'AppPublicCIDRA' : response['Items'][0]['Cidr'],
-            'AppPublicCIDRB' : response['Items'][1]['Cidr'],
-            'AppPrivateCIDRA' : response['Items'][2]['Cidr'],
-            'AppPrivateCIDRB' : response['Items'][3]['Cidr']
-        }
-        sendresponse(event, context, 'SUCCESS', responsedata, "N/A")
-    else:
-        sendresponse(event, context, 'FAILED', responsedata, "Create Failed .. Misc")
+    return cidr in reserved1, reserved2
