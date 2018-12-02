3021836eba07f8d4f0af4c066cf34799580dd763
Tony Han <itony9401@live.com>
2018-12-01 16:15:34 -0800

    finished auto database

--- wordpress-datacenter/custom-resources/auto-database.py
+++ wordpress-datacenter/custom-resources/auto-database.py
@@ -12,35 +12,95 @@ from uuid import uuid4
 import requests
 import mysql.connector as mariadb
 
+config = {
+  'user': os.environ['DB_USER'],
+  'password': os.environ['DB_PASSWORD'],
+  'host': os.environ['DB_ENDPOINT'],
+  'use_pure': True
+}
+
 def handler(event, context):
-    """Create new database and user for Wordpress application in MariaDB"""
+    """Manages databases and users for Wordpress application in MariaDB"""
     print(f"Function ARN: {context.invoked_function_arn}")
     print(f"Event: {json.dumps(event)}")
 
-    request_type = event['RequestType']
-
-    if request_type == 'Delete':
-        send_response(event, status='SUCCESS')
-        exit(1)
+    try:
+        global mariadb_connection, cursor
+        mariadb_connection = mariadb.connect(**config)
+        cursor = mariadb_connection.cursor()
+    except mariadb.Error as err:
+        print(f"Error: {err}")
+        send_response(event, status='FAILED', reason="Error connecting to database")
 
     try:
-        db_endpoint = os.environ['DB_ENDPOINT']
-        db_name = os.environ['DB_NAME']
-        db_user = os.environ['DB_USER']
-        db_password = os.environ['DB_PASSWORD']
+        request_type = event['RequestType']
+        # stack_name is used for both database and user name
+        stack_name = event['ResourceProperties']['StackName'].replace('-', '_')
+
+        # if deleting stack, delete the database and user for this stack
+        if request_type == 'Delete':
+            print('Deleting database and user...')
+            return delete_database(event, stack_name)
+        # otherwise create database and user for this stack
+        else:
+            print('Creating database and user...')
+            return create_database(event, stack_name)
+
+    except KeyError as err:
+        print(f"Error: {err}")
+        send_response(event, status='FAILED', reason="Error accessing data")
+    finally:
+        mariadb_connection.close()
+
+
+def create_database(event, stack_name):
+    """Create new database and user for Wordpress application in MariaDB"""
+    try:
+        vpc_mask = os.environ['VPC_CIDR'][:-2] + "255.255.0.0"
+        password = str(uuid4()).replace('-', '')
+        # using stack_name prevents against SQL injections
+        # as it only allows alphanumerical characters and "-"
+        # and the commands fails with %s param substitution because of single quotes
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {stack_name}")
+        cursor.execute("INSERT INTO mysql.user (User,Host,authentication_string,ssl_cipher,x509_issuer, x509_subject) VALUES(%s, %s, PASSWORD(%s),'','','')", (stack_name, vpc_mask, password))
+        cursor.execute("FLUSH PRIVILEGES")
+        cursor.execute(f"GRANT ALL PRIVILEGES ON {stack_name}.* TO %s@%s IDENTIFIED BY %s", (stack_name, vpc_mask, password))
+        cursor.execute("FLUSH PRIVILEGES")
+        mariadb_connection.commit()
 
         response_data = {
-            'DB_ENDPOINT': db_endpoint,
-            'DB_NAME': db_name,
-            'DB_USER': db_user,
-            'DB_PASSWORD': db_password
+            'DB_ENDPOINT': os.environ['DB_ENDPOINT'],
+            'DB_NAME': stack_name,
+            'DB_USER': stack_name,
+            'DB_PASSWORD': password
         }
 
         send_response(event, status='SUCCESS', data=response_data)
 
     except mariadb.Error as err:
         print(f"Error: {err}")
-        send_response(event, status='FAILED', reason="Database Error")
+        send_response(event, status='FAILED', reason="Error creating database and user")
+    finally:
+        mariadb_connection.close()
+
+
+def delete_database(event, stack_name):
+    try:
+        vpc_mask = os.environ['VPC_CIDR'][:-2] + "255.255.0.0"
+        # using stack_name prevents against SQL injections
+        # as it only allows alphanumerical characters and "-"
+        # and the commands fails with %s param substitution because of single quotes
+        cursor.execute(f"DROP DATABASE {stack_name}")
+        cursor.execute("DROP USER %s@%s", (stack_name, vpc_mask))
+        mariadb_connection.commit()
+
+        send_response(event, status='SUCCESS')
+
+    except mariadb.Error as err:
+        print(f"Error: {err}")
+        send_response(event, status='FAILED', reason="Error deleting database or user")
+    finally:
+        mariadb_connection.close()
 
 
 def send_response(event, status, reason="", data=None):
@@ -57,7 +117,7 @@ def send_response(event, status, reason="", data=None):
         'Reason': reason,
         'RequestId': event['RequestId'],
         'LogicalResourceId': event['LogicalResourceId'],
-        'PhysicalResourceId': event['LogicalResourceId'] + str(uuid4()),
+        'PhysicalResourceId': event.get('PhysicalResourceId', None) or event['LogicalResourceId'] + str(uuid4()),
         'Data': data
     }
 
--- wordpress-datacenter/custom-resources/auto-subnet.py
+++ wordpress-datacenter/custom-resources/auto-subnet.py
@@ -21,81 +21,25 @@ dynamodb_table = boto3.resource('dynamodb', region_name=region).Table(table_name
 
 
 def handler(event, context):
-    """Allocate 3 IP ranges from a VPC CIDR Block"""
+    """Allocate 2 IP ranges from a VPC CIDR Block"""
     print(f"Function ARN: {context.invoked_function_arn}")
     print(f"Event: {json.dumps(event)}")
 
     stack_id = event['StackId']
     request_type = event['RequestType']
 
-    # if deleting stack, remove the subnet allocationsfor this stack_id
+    # if deleting stack, remove the subnet allocations for this stack
     if request_type == 'Delete':
         print("Deleting Subnets...")
-        delete_subnets(event, stack_id)
-        exit(1)
-
-    ## otherwise allocate 4 ranges
-    print("Allocating Subnets...")
-    ipnet = IPNetwork(vpc_cidr)
-    subnet_mask = 24
-    subnets = ipnet.subnet(subnet_mask)
-    subnets_allocated = 0
-    number_to_allocate = 2
-
-    for subnet in subnets:
-        if subnets_allocated == number_to_allocate:
-            break
-        if cidr_is_reserved(subnet):
-            continue
-        if cidr_is_in_table(subnet):
-            continue
-
-        use_subnet(subnet, stack_id)  # store the allocation in the DB
-        print(f"Subnets Allocated {subnets_allocated} - {str(subnet)}")
-        subnets_allocated += 1
-
-    # read all the subnets for this stack from table
-    response = dynamodb_table.query(
-        IndexName='StackCidrRanges',
-        KeyConditionExpression=Key('StackId').eq(stack_id))
-
-    if len(response['Items']) < number_to_allocate:
-        send_response(
-            event,
-            status='FAILED',
-            reason=f"Couldn't obtain {number_to_allocate} CIDR for stack from DynamoDB query"
-        )
-        exit(1)
-
-    print('Response from DynamoDB: ')
-    for item in response['Items']:
-        print(item)
-
-    try:
-        response_data = {
-            'AppPublicCIDR1': response['Items'][0]['Cidr'],
-            'AppPublicCIDR2': response['Items'][1]['Cidr'],
-        }
-        send_response(event, status='SUCCESS', data=response_data)
-
-    except Exception as err:
-        send_response(
-            event, status='FAILED', reason="Failed to send response data")
-        print(f"Error: {err}")
-
-
-def use_subnet(cidr, stack_id):
-    """ Add the passed subnet into the DB"""
-    cidr = str(cidr)
-
-    dynamodb_table.put_item(Item={
-        'Cidr': cidr,
-        'StackId': str(stack_id),
-    })
+        return delete_subnets(event, stack_id)
+    # otherwise allocate subnets for this stack
+    else:
+        print("Allocating Subnets...")
+        return allocate_subnets(event, stack_id)
 
 
 def delete_subnets(event, stack_id):
-    """ Delete any subnets in the DB for a stack"""
+    """Delete any subnets in DynamoDB for stack"""
     try:
         response = dynamodb_table.scan(
             FilterExpression=Attr('StackId').eq(stack_id))
@@ -108,11 +52,88 @@ def delete_subnets(event, stack_id):
         send_response(event, status='SUCCESS')
 
     except Exception as err:
+        print(f"Error: {err}")
         send_response(
             event,
             status='FAILED',
             reason='Failed to delete allocated subnets in DynamoDB')
+
+
+def allocate_subnets(event, stack_id):
+    """Allocate subnets in DynamoDB for stack"""
+    ipnet = IPNetwork(vpc_cidr)
+    subnet_mask = 24
+    subnets = ipnet.subnet(subnet_mask)
+    subnets_allocated = 0
+    number_to_allocate = 2
+
+    try:
+        for subnet in subnets:
+            if subnets_allocated == number_to_allocate:
+                break
+            if cidr_is_reserved(subnet):
+                continue
+            if cidr_is_in_table(subnet):
+                continue
+
+            use_subnet(subnet, stack_id)  # store the allocation in the DB
+            print(f"Subnets Allocated {subnets_allocated} - {str(subnet)}")
+            subnets_allocated += 1
+
+        # read all the subnets for this stack from table
+        response = dynamodb_table.query(
+            IndexName='StackCidrRanges',
+            KeyConditionExpression=Key('StackId').eq(stack_id))
+
+        if len(response['Items']) < number_to_allocate:
+            send_response(
+                event,
+                status='FAILED',
+                reason=f"Couldn't obtain {number_to_allocate} CIDR for stack from DynamoDB query"
+            )
+            return
+
+        print('Response from DynamoDB: ')
+        for item in response['Items']:
+            print(item)
+
+        response_data = {
+            'AppPublicCIDR1': response['Items'][0]['Cidr'],
+            'AppPublicCIDR2': response['Items'][1]['Cidr'],
+        }
+        send_response(event, status='SUCCESS', data=response_data)
+
+    except Exception as err:
+        send_response(
+            event, status='FAILED', reason="Failed allocate subnets")
         print(f"Error: {err}")
+
+
+def use_subnet(cidr, stack_id):
+    """Add the subnet to DynamoDB"""
+    cidr = str(cidr)
+    dynamodb_table.put_item(Item={
+        'Cidr': cidr,
+        'StackId': str(stack_id),
+    })
+
+
+def cidr_is_in_table(cidr):
+    """Check if the CIDR is already allocated """
+    cidr = str(cidr)
+    response = dynamodb_table.query(
+        KeyConditionExpression=Key('Cidr').eq(cidr))
+    items = response['Items']
+
+    return len(items) > 0
+
+
+def cidr_is_reserved(cidr):
+    """Check if CIDR is already reserved"""
+    cidr = str(cidr)
+    reserved_cidrs = json.loads(os.getenv('RESERVED_CIDRS', '[]'))
+
+    return cidr in reserved_cidrs
 
 
 def send_response(event, status, reason="", data=None):
@@ -129,7 +150,7 @@ def send_response(event, status, reason="", data=None):
         'Reason': reason,
         'RequestId': event['RequestId'],
         'LogicalResourceId': event['LogicalResourceId'],
-        'PhysicalResourceId': event['LogicalResourceId'] + str(uuid4()),
+        'PhysicalResourceId': event.get('PhysicalResourceId', None) or event['LogicalResourceId'] + str(uuid4()),
         'Data': data
     }
 
@@ -137,21 +158,3 @@ def send_response(event, status, reason="", data=None):
 
     print(f"Sending {json_payload} to {event['ResponseURL']}")
     requests.put(event['ResponseURL'], data=json_payload)
-
-
-def cidr_is_in_table(cidr):
-    """ Check if the CIDR is already allocated """
-    cidr = str(cidr)
-    response = dynamodb_table.query(
-        KeyConditionExpression=Key('Cidr').eq(cidr))
-    items = response['Items']
-
-    return len(items) > 0
-
-
-def cidr_is_reserved(cidr):
-    """ Check if CIDR is already reserved"""
-    cidr = str(cidr)
-    reserved_cidrs = json.loads(os.getenv('RESERVED_CIDRS', '[]'))
-
-    return cidr in reserved_cidrs
